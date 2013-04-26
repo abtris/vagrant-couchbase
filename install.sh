dffc1b99970e0b2b4c598f2f94869626825361fe
Ladislav Prskavec <ladislav@prskavec.net>
2013-04-26 09:22:59 +0200

    Initial import

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file="/vagrant/couchbase-server-enterprise_x86_2.0.1.deb"
+if [ -f "$file" ]
+then
+      echo "$file found."
+else
+      echo "$file not found."
+      wget http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_2.0.1.deb
+fi
+sudo apt-get install libssl0.9.8
+sudo dpkg -i couchbase-server-enterprise_x86_2.0.1.deb 
+         
---
+++ readme.md
@@ -0,0 +1,5 @@
+# CouchBase cluster 
+
+Install [vagrant](http://www.vagrantup.com/)
+
+Download package for install: [http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_2.0.1.deb](http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_2.0.1.deb)
