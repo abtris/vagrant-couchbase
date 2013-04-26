14188224a433ae8be1500d00d5cf0a0fc7ee24e4
Ladislav Prskavec <ladislav@prskavec.net>
2013-04-26 11:23:12 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ else
       echo "$file not found."
       wget http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_2.0.1.deb
 fi
-sudo apt-get install libssl0.9.8
+sudo apt-get install -y libssl0.9.8
 sudo dpkg -i couchbase-server-enterprise_x86_2.0.1.deb 
          
